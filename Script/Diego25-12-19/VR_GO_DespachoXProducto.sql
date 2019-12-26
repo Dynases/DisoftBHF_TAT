fe104651be4b941455d63973aaa24fb259fb5708
Diego <dcayo.156@gmail.com>
2019-12-26 12:59:05 -0400

    Diego Scrip DespachoxProducto

--- Script/Diego25-12-19/VR_GO_DespachoXProducto.sql
+++ Script/Diego25-12-19/VR_GO_DespachoXProducto.sql
@@ -1,43 +1,203 @@
-SELECT        e.oaccbnumi, c.canumi, c.cacod, c.cadesc, c.cadesc2, d.cadesc AS categoria, SUM(b.obpcant) AS obpcant, F.chprecio, SUM(b.obpcant) * F.chprecio AS Total,e.oacnconc
+USE [BDDistBHF_TAT]
+GO
+
+/****** Object:  View [dbo].[VR_GO_DespachoXProducto]    Script Date: 26/12/2019 12:57:12 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[VR_GO_DespachoXProducto]
+AS
+SELECT        e.oaccbnumi, c.canumi, c.cacod, c.cadesc, c.cadesc2, d.cadesc AS categoria, SUM(b.obpcant) AS obpcant, f.chprecio, SUM(b.obpcant) * f.chprecio AS Total
 FROM            dbo.TO001 AS a INNER JOIN
                          dbo.TO0011 AS b ON a.oanumi = b.obnumi INNER JOIN
                          dbo.TC001 AS c ON b.obcprod = c.canumi INNER JOIN
                          dbo.TC005C AS d ON c.cacat = d.canumi INNER JOIN
                          dbo.TO001C AS e ON a.oanumi = e.oacoanumi INNER JOIN
-						 dbo.TC003 AS f ON f.chcprod = C.canumi AND F.chcatcl = 2
-WHERE  e.oaccbnumi = 10 and a.oaest < 3
-GROUP BY e.oaccbnumi, c.canumi, c.cacod, c.cadesc, c.cadesc2, d.cadesc, f.chprecio,e.oacnconc
+                         dbo.TC003 AS f ON f.chcprod = c.canumi AND f.chcatcl = 2
+WHERE        (a.oaest < 3)
+GROUP BY e.oaccbnumi, c.canumi, c.cacod, c.cadesc, c.cadesc2, d.cadesc, f.chprecio
+
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "a"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 136
+               Right = 262
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "b"
+            Begin Extent = 
+               Top = 138
+               Left = 38
+               Bottom = 268
+               Right = 262
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "c"
+            Begin Extent = 
+               Top = 270
+               Left = 38
+               Bottom = 400
+               Right = 262
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "d"
+            Begin Extent = 
+               Top = 402
+               Left = 38
+               Bottom = 532
+               Right = 262
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "e"
+            Begin Extent = 
+               Top = 534
+               Left = 38
+               Bottom = 664
+               Right = 262
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "f"
+            Begin Extent = 
+               Top = 6
+               Left = 300
+               Bottom = 136
+               Right = 525
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 10
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 234' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'VR_GO_DespachoXProducto'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane2', @value=N'0
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 12
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'VR_GO_DespachoXProducto'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=2 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'VR_GO_DespachoXProducto'
+GO
 
 
-SELECT * FROM TO0011
-SELECT * FROM TO001C
-SELECT * FROM TC003 WHERE chcatcl = 2 --Precio
-SELECT * FROM TC001
-
-SELECT        e.oaccbnumi, c.canumi, c.cacod, c.cadesc, c.cadesc2, d.cadesc AS categoria, SUM(b.obpcant) AS obpcant,e.oacnconc
-FROM            dbo.TO001 AS a INNER JOIN
-                         dbo.TO0011 AS b ON a.oanumi = b.obnumi INNER JOIN
-                         dbo.TC001 AS c ON b.obcprod = c.canumi INNER JOIN
-                         dbo.TC005C AS d ON c.cacat = d.canumi INNER JOIN
-                         dbo.TO001C AS e ON a.oanumi = e.oacoanumi 
-GROUP BY e.oaccbnumi, c.canumi, c.cacod, c.cadesc, c.cadesc2, d.cadesc,e.oacnconc
-
-SELECT *  FROM  TC002 A WHERE A.cbcat = 1
-
-SELECT        e.oaccbnumi, c.canumi, c.cacod, c.cadesc, c.cadesc2, d.cadesc AS categoria, SUM(b.obpcant) AS obpcant, B.obpbase,SUM(b.obptot) as Total
-FROM            dbo.TO001 AS a INNER JOIN
-                         dbo.TO0011 AS b ON a.oanumi = b.obnumi INNER JOIN
-                         dbo.TC001 AS c ON b.obcprod = c.canumi INNER JOIN
-                         dbo.TC005C AS d ON c.cacat = d.canumi INNER JOIN
-                         dbo.TO001C AS e ON a.oanumi = e.oacoanumi 
-WHERE  e.oaccbnumi = 10 and a.oaest < 3
-GROUP BY e.oaccbnumi, c.canumi, c.cacod, c.cadesc, c.cadesc2, d.cadesc, b.obpbase,b.obptot
-
-SELECT        e.oaccbnumi, c.ccnumi, c.cccod, c.ccdesc, ISNULL(b.oacnrofac, 0) AS oacnrofact, SUM(d.obptot) AS obptot
-FROM            dbo.TO001 AS a INNER JOIN
-                         dbo.TO001C AS b ON a.oanumi = b.oacoanumi INNER JOIN
-                         dbo.TC004 AS c ON a.oaccli = c.ccnumi INNER JOIN
-                         dbo.TO0011 AS d ON a.oanumi = d.obnumi INNER JOIN
-                         dbo.TO001C AS e ON a.oanumi = e.oacoanumi
-WHERE a.oaest < 3
-GROUP BY e.oaccbnumi, c.ccnumi, c.cccod, c.ccdesc, b.oacnrofac
